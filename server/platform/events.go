7a6b418472b0fe7e4ef66254314dccfd64f2f62f
dustin-ward <wardddustin@gmail.com>
2024-11-24 11:03:26 -0500

    Fix Server

--- server/platform/events.go
+++ server/platform/events.go
@@ -1,12 +1,17 @@
 package platform
 
-import "github.com/Jewels2001/LlamaFeeders/server/inference"
+import (
+	"log"
+
+	"github.com/Jewels2001/LlamaFeeders/server/inference"
+)
 
 func GenerateEventPost() (Post, error) {
     messages := []inference.OllamaMsg{
         {Role: "system", Content: inference.PROMPT_EVENT_GENERATION},
         {Role: "user", Content: "Generate an event like in your instructions."},
     }
+    log.Println("EVENT:", messages)
 
     msg, err := inference.GetChatCompletion(messages)
     if err != nil {
