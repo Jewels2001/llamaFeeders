9b68077f1a43468e0cb5826c549a8d00d027f9bd
Jewels2001 <juliewq25@gmail.com>
2024-11-23 21:03:58 -0500

    add: basic getEvents, getUserData, postUserEvent

--- http_instance/main.go
+++ http_instance/main.go
@@ -10,7 +10,9 @@ import (
 
 
 func main() {
-	http.HandleFunc("/api/", getEvents)
+	http.HandleFunc("/api/createNewEvent", postUserEvent)
+	http.HandleFunc("/api/getEvents", getEvents)
+	http.HandleFunc("/api/getUserData", getUserData)
 	// server := &http.Server{
 	// 	Addr: fmt.Sprintf(":8080/api"),
 	// 	Handler: handers.New(),
@@ -24,18 +26,59 @@ func main() {
 	}
 }
 
-// func (s *server) ServeHTTP (w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.WriteHeader(http.StatusOK)
-// 	w.Write([]byte(`{"message": "hello :)}`))
-// }
+dataFeed = `{[
+        {
+                "username": "Bookworm95",
+                "comments" : "Nice post",
+                "commentId" :  123
+        },
+        {
+                "username": "Bookworm96",
+                "comments" : "Nice post!!!!!",
+                "commentId" :  124
+        },
+        ]}`
+
+dataUser = `
+{
+    "username" : "BookWorm96",
+    "age": 25,
+    "occupation": "Software Engineer",
+    "interests": ["reading", "coding", "gaming"],
+    "personality": {
+        "oe": 0.8,
+        "co": 0.3,
+        "ex": 0.5,
+        "ag": 0.7,
+        "ne": 0.6
+    },
+    "education": "Bachelors in Computer Science",
+    "profilePicture": ""
+}
+`
 
 func getEvents(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "Welcome to the getEvents endpoint!"}`))
+	w.Write([]byte(dataFeed))
 	// fmt.Fprintf(w, "Welcome to the getEvents endpoint!") // w is response, string is data
 	fmt.Println("Endpoint Hit: getEvents")
+}
+
+func getUserData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(dataUser))
+	fmt.Println("Endpoint Hit: getUserData")
+}
+
+
+func postUserEvent(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message": "post called"}`))
+	fmt.Println("Endpoint Hit: postUserEvent")
+
 }
 
 
