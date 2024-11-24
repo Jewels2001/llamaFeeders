34e353dcc4cdb9b7e6e5c61e41725151241d4df2
dustin-ward <wardddustin@gmail.com>
2024-11-23 20:20:53 -0500

    System events

--- server/inference/prompts.go
+++ server/inference/prompts.go
@@ -1,9 +1,10 @@
 package inference
 
-const PROMPT_AGENT_CREATION = `Create properties of ONE person using the big 5 personality traits, an age, and occupation, an education, provide a list of 7 interests. Based on their personality give them a likelihood to respond to any given post. Also provide a probability range that this person will respond to a post related to their interests. Ensure that the probability range for topics of interest are higher than the standard probability.
+const PROMPT_AGENT_CREATION = `Create properties of ONE person using the big 5 personality traits, an age, and occupation, an education, provide a list of up to 7 interests. Based on their personality give them a likelihood to respond to any given post. Also provide a probability range that this person will respond to a post related to their interests. Ensure that the probability range for topics of interest are higher than the standard probability.
 
 DO NOT respond with any text other than the person requested.
 DO NOT create more than 1 person.
+Create unique usernames that can contain numbers.
 
 Respond in JSON of the form:
 {
@@ -19,6 +20,13 @@ Respond in JSON of the form:
 "likelihood_top": LikelihoodTop
 }`
 
+const PROMPT_EVENT_GENERATION = `You are LLAMANEWS, an all-seeing reporter on news. You have no biases and report fictional yet realistic events as if they were true. The category is not important. You are reporting on these events in real-time to a social media feed. You are a native speaker of the English language and will report on events in English. 
+- DO NOT mention LLAMANEWS in your post
+- DO NOT describe your task in the generated texts.
+- DO NOT provide a perspective in the content.
+- Events can be both positive uplifting stories, boring facts, or negative events.
+- All generated texts MUST be short (up to 200 characters).`
+
 const PROMPT_AGENT_SYSTEM = `Your username is @%s. You are a %d year old %s %s with the education level: %s. You are interested in %s. Your Big Five personality traits are: %f, %f, %f, %f and %f. Your likelihood to respond to a post is %f in general and between %f-%f for posts related to your interests.
 
 Act as requested by the God
@@ -32,6 +40,7 @@ Act as requested by the God
 const PROMPT_AGENT_POST_FORMAT = `Read the following feed and write a Tweet that may or may not be responding to another tweet in the feed. Tweets are separated by newlines and contain one AUTHOR
 
 - You can tag the author of the tweet using @ if they are not LLAMANEWS.
+- Posts from author: LLAMANEWS are more interesting and should be replied to more often.
 - Be consistent with your Big Five personality traits.
 - Avoid excessive politeness.
 - Your comment may or may not contribute to the conversation.
--- server/main.go
+++ server/main.go
@@ -9,6 +9,7 @@ import (
 )
 
 const NumAgents = 10
+const MaxPostsLen = 50
 
 func main() {
     log.Println("Generating Agents...")
@@ -17,17 +18,30 @@ func main() {
     log.Println("Done.")
 
     var posts []platform.Post
-    for _, agent := range(agent.Agents) {
-        log.Printf("Generating post for user: %s", agent.Username)
-        post, err := agent.GeneratePost(posts)
+    for {
+        log.Println("Generating new platform event...")
+        eventPost, err := platform.GenerateEventPost()
         if err != nil {
-            log.Printf("Error generating post for user %s: %v", agent.Username, err)
+            log.Printf("Error generating event post: %v", err)
+        }
+        log.Println(eventPost.String())
+        posts = append(posts, eventPost)
+
+        for _, curAgent := range(agent.Agents) {
+            log.Printf("Generating post for user: %s", curAgent.Username)
+            post, err := curAgent.GeneratePost(posts)
+            if err != nil {
+                log.Printf("Error generating post for user %s: %v", curAgent.Username, err)
+            }
+
+            if post.Message != "NO_RESPONSE" {
+                fmt.Println(post.String())
+                posts = append(posts, post)
+            }
         }
 
-        fmt.Println(post)
-
-        if post.Message != "NO_RESPONSE" {
-            posts = append(posts, post)
+        if len(posts) > MaxPostsLen {
+            posts = posts[1:]
         }
     }
 }
