9bffe4cc72f9b12e24695fc07950632d555c0e74
dustin-ward <wardddustin@gmail.com>
2024-11-23 20:20:53 -0500

    System events

---
+++ server/platform/events.go
@@ -0,0 +1,17 @@
+package platform
+
+import "github.com/Jewels2001/LlamaFeeders/server/inference"
+
+func GenerateEventPost() (Post, error) {
+    messages := []inference.OllamaMsg{
+        {Role: "system", Content: inference.PROMPT_EVENT_GENERATION},
+        {Role: "user", Content: "Generate an event like in your instructions."},
+    }
+
+    msg, err := inference.GetChatCompletion(messages)
+    if err != nil {
+        return Post{}, err
+    }
+
+    return Post{Author: "LLAMANEWS", Message: msg.Content}, nil
+}
