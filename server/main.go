ac66be4cdde0582011caa875109a23ce432f9702
dustin-ward <wardddustin@gmail.com>
2024-11-24 02:09:43 -0500

    CreateNewEvent endpoint

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/Jewels2001/LlamaFeeders/server/agent"
 	"github.com/Jewels2001/LlamaFeeders/server/platform"
@@ -13,6 +14,7 @@ import (
 const NumAgents = 10
 const MaxPostsLen = 10
 
+var mu sync.Mutex
 var posts []platform.Post
 
 func main() {
@@ -29,7 +31,9 @@ func main() {
                 log.Printf("Error generating event post: %v", err)
             }
             log.Println(eventPost.String())
+            mu.Lock()
             posts = append(posts, eventPost)
+            mu.Unlock()
 
             for _, curAgent := range(agent.Agents) {
                 // log.Printf("Generating post for user: %s", curAgent.Username)
@@ -40,17 +44,22 @@ func main() {
 
                 if post.Message != "NO_RESPONSE" {
                     fmt.Println(post.String())
+                    mu.Lock()
                     posts = append(posts, post)
+                    mu.Unlock()
                 }
             }
 
             if len(posts) > MaxPostsLen {
+                mu.Lock()
                 posts = posts[len(posts)-MaxPostsLen:]
+                mu.Unlock()
             }
         }
     }()
 
     http.HandleFunc("/api/getEvents", getEventsEndpoint)
+    http.HandleFunc("/api/createNewEvent", createNewEvent)
     log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
@@ -67,4 +76,29 @@ func getEventsEndpoint(w http.ResponseWriter, r* http.Request) {
     w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
     w.WriteHeader(http.StatusOK)
     w.Write(data)
+}
+
+func createNewEvent(w http.ResponseWriter, r* http.Request) {
+    type Event struct{
+        Text string `json:"eventText"`
+    }
+    var newEvent Event
+
+    if err := json.NewDecoder(r.Body).Decode(&newEvent); err != nil {
+        log.Println("Error creating new event:", err)
+        w.WriteHeader(http.StatusInternalServerError)
+    }
+
+    post := platform.NewPost("LLAMANEWS", newEvent.Text)
+    mu.Lock()
+    posts = append(posts, post)
+    mu.Unlock()
+    log.Println(post.String())
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("Event sucesfully created"))
 }
